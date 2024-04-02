4d980c5e61d4d1bb9f3560a7f244e6a7fbacb36b
code-tieumomo <code.tieumomo@gmail.com>
2024-04-02 15:33:03 +0700

    feat: speed insight

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from "~/components/common/Header.vue";
 import Navbar from "~/components/common/Navbar.vue";
+import { SpeedInsights } from "@vercel/speed-insights/vue";
 </script>
 
 <template>
+    <SpeedInsights/>
     <Header/>
     <Navbar/>
     <main>
